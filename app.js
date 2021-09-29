ad06a6338a69c430fa920cb60023d697dc7dd864
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-09-29 16:10:40 -0700

    working on keyframs

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 const catImg = document.getElementById('cat-img');
 const dogImg = document.getElementById('dog-img');
 const horseImg = document.getElementById('horse-img');
+
+
+catImg.addEventListener('click', () => {
+    let catAudio = document.getElementById('cat-sound');
+    console.log('click');
+    catAudio.play();
+    catImg.classList.add('animated');
+});
+
+dogImg.addEventListener('click', () =>{
+    let dogAudio = document.getElementById('dog-sound');
+    dogAudio.play();
+
+});
+horseImg.addEventListener('click', () =>{
+    let horseAudio = document.getElementById('horse-sound');
+    horseAudio.playbackRate = 0.5;
+    horseAudio.play();
+
+});
+
